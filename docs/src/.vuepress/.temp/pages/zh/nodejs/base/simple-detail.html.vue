888e5a89c9ac650f56c94efb3e78d04f62dd8f13
SaraiNoQ <2994498974@qq.com>
2022-10-31 14:43:24 +0800

    fix: remote cache and temp

--- docs/src/.vuepress/.temp/pages/zh/nodejs/base/simple-detail.html.vue
+++
@@ -1,4 +0,0 @@
-<template><div><h1 id="nodejs入门" tabindex="-1"><a class="header-anchor" href="#nodejs入门" aria-hidden="true">#</a> Nodejs入门</h1>
-</div></template>
-
-
--- docs/src/.vuepress/.temp/pages/zh/posts/nodejs/index.html.vue
+++
@@ -1,4 +0,0 @@
-<template><div><h1 id="nodejs" tabindex="-1"><a class="header-anchor" href="#nodejs" aria-hidden="true">#</a> Nodejs</h1>
-</div></template>
-
-
